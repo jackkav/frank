f6188ff290e4f543bdb998c37f9456a2af011153
Jack Kavanagh <jackkav@gmail.com>
2019-11-19 17:07:56 +0100

    add bmi function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
 pub fn farenheit(celsius: i32) -> i32 {
     (((celsius as f32) - 32.) * 5. / 9.) as i32
 }
+
+pub fn bmi(weight: u32, height: f32) -> &'static str {
+    let bmi = weight as f32 / height.powf(2.0);
+    if bmi <= 18.5 {
+        return "Underweight";
+    }
+    if bmi <= 25.0 {
+        return "Normal";
+    }
+    if bmi <= 30.0 {
+        return "Overweight";
+    }
+    "Obese"
+}
--- tests/test_import.rs
+++ tests/test_import.rs
@@ -4,3 +4,11 @@ fn test_farenheit() {
     assert_eq!(10, frank::farenheit(50));
     assert_eq!(22, frank::farenheit(72))
 }
+
+#[test]
+fn test_bmi() {
+    assert_eq!(frank::bmi(50, 1.80), "Underweight");
+    assert_eq!(frank::bmi(80, 1.80), "Normal");
+    assert_eq!(frank::bmi(90, 1.80), "Overweight");
+    assert_eq!(frank::bmi(110, 1.80), "Obese");
+}
